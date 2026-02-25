21f2a59b08f70aad9626b1e85e82bb89cb5e80cc
Fernando Asato <fp.asato.dev@gmail.com>
2026-02-25 19:33:48 -0300

    20/02

--- desafio.py
+++ desafio.py
@@ -2,10 +2,11 @@
 with open("nomes_ficticios.txt", "r", encoding="utf-8") as arquivo:
     nomes = arquivo.read().split("\n")
     
-    nomes = [nome.lower() for nome in nomes]
-    
+    nomes = [nome.lower() for nome in nomes]    
     nomes.sort()
     
     with open("nomes_tratados.txt", "w", encoding="utf-8") as arquivo:
         for nome in nomes:
             arquivo.write(f"{nome}\n")
+
+
